a7eab74e3af38775627f7f6af0009e3c18742377
Thomas Steinkamp <thomas.steinkamp@home>
2026-05-31 22:15:03 +0200

    further develop skeleton, this time for embedded (no-std) code on raspberry pico. Mostly inherited from my rpico+embassy template.

--- pcd8544/src/lib.rs
+++ pcd8544/src/lib.rs
@@ -1,14 +1,19 @@
+#![no_std]
+
+use defmt::*;
+
 pub fn add(left: u64, right: u64) -> u64 {
+    info!("in fn add");
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
