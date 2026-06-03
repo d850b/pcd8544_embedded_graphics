6dfb92afcb52544708f1db22d1644de519a0aef0
Thomas Steinkamp <thomas.steinkamp@home>
2026-06-03 17:13:52 +0200

    add code to create an spi device to rp_simple/src/main.rs we will need it to access the display.

--- examples/rp/rp_simple/src/main.rs
+++ examples/rp/rp_simple/src/main.rs
@@ -1,7 +1,3 @@
-//! This example test the RP Pico on board LED.
-//!
-//! It does not work with the RP Pico W board. See wifi_blinky.rs.
-
 #![no_std]
 #![no_main]
 
@@ -9,19 +5,59 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_rp::{bind_interrupts, dma};
+use embassy_rp::peripherals::{DMA_CH0, DMA_CH1};
+
 use embassy_rp::gpio;
 use embassy_time::Timer;
 use gpio::{Level, Output};
+
+// SPI stuff
+use embassy_rp::spi::Spi;
+use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
+
+// Mutex stuff.
+use embassy_sync::blocking_mutex::raw::{CriticalSectionRawMutex, NoopRawMutex};
+use embassy_sync::mutex::Mutex;
+
+
 use {defmt_rtt as _, panic_probe as _};
+
+
+bind_interrupts!(struct Irqs {
+    DMA_IRQ_0 => dma::InterruptHandler<DMA_CH0>, dma::InterruptHandler<DMA_CH1>;
+});
+
 
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-
-    info!("Hello World. pcd8544::add(42, 43) returns {}", pcd8544::add(42, 43));
-
     let p = embassy_rp::init(Default::default());
     let mut led = Output::new(p.PIN_25, Level::Low);
+
+    //
+    // set up spi
+    //
+    
+    info!("set up spi and io's for pcd8544 display");
+
+    let used_spi = p.SPI1;
+    let sx_miso = p.PIN_12;
+    let sx_mosi = p.PIN_11;
+    let sx_clk = p.PIN_10;
+    let sx_csn = p.PIN_13;
+    let sx_dc = p.PIN_14;
+
+    let sx_cs_output = Output::new(sx_csn, Level::High); // Initially disable!
+    let sx_dc_output = Output::new(sx_dc, Level::Low);
+
+    // create spi bus, wrap it in a mutex.
+    let spi_bus = Spi::new(used_spi, sx_clk, sx_mosi, sx_miso, p.DMA_CH0, p.DMA_CH1, Irqs, embassy_rp::spi::Config::default());
+    let spi_bus_mutex = Mutex::<NoopRawMutex, _>::new(spi_bus);
+
+    // create spi device 
+    let spi_device = SpiDevice::new(&spi_bus_mutex, sx_cs_output);
+
 
     loop {
         info!("led on!");
